27bf9e0a2fed4e9efa6d3b53c5b451583944b449
Brentph505 <abrazaldo1989@gmail.com>
2024-10-09 23:45:13 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -211,7 +211,7 @@ const Home = () => {
   }, [itemsCount]);
 
   useEffect(() => {
-    document.title = `Miruro | Watch Anime Online, Free Anime Streaming`;
+    document.title = `Zenime | Watch Anime Online, Free Anime Streaming`;
   }, [activeTab]);
 
   useEffect(() => {
